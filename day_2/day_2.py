9effb38c930e7c9d1b139eed8a60f8ccd10700d1
Manish Lohani <manish.lohani05@gmail.com>
2024-12-04 05:51:10 +0530

    refactor  comments

--- day_2/day_2.py
+++ day_2/day_2.py
@@ -13,19 +13,22 @@ except FileNotFoundError:
 # ---------------------------------------
 
 games = contents.splitlines()
-full_game = []  # variable to store info of all game trials
+full_game = []  # list to store detailed info of all game trials
 
+# Process each game's data
 for game in games:
-    # Split the line into 2 parts e.g, Game id, sets of game
-    game_id, game_sets_str = game.split(": ")
-    game_number = int(
-        game_id.split(" ")[1]
-    )  # Game 1: 3 blue, 4 red; 1 red, 2 green => game_number = 1
-    game_sets = game_sets_str.split(
-        "; "
-    )  # ['3 blue, 4 red', '1 red, 2 green, 6 blue', '2 green']
 
-    # iteration for each set of the game
+    # Split the line into game Id and associated sets (e.g., "Game 1: 3 blue, 4 red; 1 red, 2 green")
+    game_id, game_sets_str = game.split(": ")
+
+    # Extract game number from  the game Id string (e.g., "Game 1" -> 1)
+    game_number = int(game_id.split(" ")[1])
+
+    # Split the game sets string into individual sets
+    # (e.g., "3 blue, 4 red; 1 red, 2 green" -> ['3 blue, 4 red', '1 red, 2 green'])
+    game_sets = game_sets_str.split("; ")
+
+    # Iteration over each set of the game and store the info of this set into dictionary
     for i, each_set in enumerate(game_sets):
         game_set_dict = {
             "game number": game_number,
@@ -35,15 +38,16 @@ for game in games:
             "blue": 0,
         }
 
-        # Extract the color of cubes and their counts in each set e.g, ['3 blue', '4 red']
+        # Extract the color of cubes and cooresponding counts within each set and store them in the dictionay
         for cube in each_set.split(", "):
             color_count = int(cube.split(" ")[0])
             color = cube.split(" ")[1]
             game_set_dict[color] = color_count
 
+        # Append the set information dictionary into full_game list
         full_game.append(game_set_dict)
 
-# Dataframe of all game trials
+# Create Dataframe of all game trials
 df = pd.DataFrame(full_game)
 
 # Sum of valid game ids
@@ -57,7 +61,6 @@ print(f"The sum of valid game ids is {sum_valid_game_ids}.")
 # ---------------------------------------------------------------------------------------------
 
 sum_power_of_sets = calculate_power_of_minimum_set(df)
-
 print(
     f"The sum of the power of sets which contain the minimum number of cubes that must have been present is {sum_power_of_sets}."
 )
